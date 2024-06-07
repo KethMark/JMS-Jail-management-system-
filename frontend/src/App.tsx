745ad29e460b051c6752ac81d1c01e086a75920b
Keth Mark <aseronberou@gmail.com>
2024-06-07 10:32:31 +0800

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import AuthRoute from "./routes/AuthRoute";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./components/auth/sign-in";
+import { SignUp } from "./components/auth/sign-up";
 
 function App() {
   return (
     <>
-      <AuthRoute />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </>
-  );
+  )
 }
 
 export default App;
